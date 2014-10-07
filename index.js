8795ef872c72f9432452f0e55cfb5bd9241724e3
thesbros <oden@thesbros.me>
2014-10-07 01:24:44 -0700

    Add opts, forgot that

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var spawn = require("child_process").spawn;
 
-module.exports.measure = function(callback)
+module.exports.measure = function(callback, opts)
 {
 	var regex = /volt=([^V]+)/;
-	var cmd = spawn("/opt/vc/bin/vcgencmd", ["measure_volts"]);
+	var cmd = spawn("/opt/vc/bin/vcgencmd", ["measure_volts", opts.id || "core"]);
 	cmd.stdout.on("data", function(buf)
 	{
 		callback(Number(regex.exec(buf.toString("utf8"))[1]))
