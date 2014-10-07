d59b8a5f2314e4aad164beea6b8706c348816524
thesbros <oden@thesbros.me>
2014-10-07 01:28:58 -0700

    I'm a derp

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var spawn = require("child_process").spawn;
 
 module.exports.measure = function(callback, opts)
 {
+	var id = opts ? opts.id || "core" : "core";
 	var regex = /volt=([^V]+)/;
-	var cmd = spawn("/opt/vc/bin/vcgencmd", ["measure_volts", opts.id || "core"]);
+	var cmd = spawn("/opt/vc/bin/vcgencmd", ["measure_volts", id]);
 	cmd.stdout.on("data", function(buf)
 	{
 		callback(Number(regex.exec(buf.toString("utf8"))[1]))
